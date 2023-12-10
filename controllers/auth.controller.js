274a9811db23b711ed071d74732443238d8db805
Shreyansh-deep <shreyanshdeep4@gmail.com>
2023-12-10 13:01:00 +0530

    adds signin route

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,15 +1,69 @@
 const { query } = require('../services/db');
-const { comparePassword } = require('../utils/auth.utils');
+const { comparePassword, hashPassword } = require('../utils/auth.utils');
+
+exports.signUp = async (req, res) => {
+  const { username, password, email } = req.body;
+
+  if (!email)
+    return res.status(400).json({ status: 'fail', message: 'email missing' });
+
+  if (!username)
+    return res
+      .status(400)
+      .json({ status: 'fail', message: 'username missing' });
+
+  if (!password)
+    return res
+      .status(400)
+      .json({ status: 'fail', message: 'password missing' });
+
+  // find user with email
+  const userWithEmail = await query(`SELECT * FROM user WHERE email =?`, [
+    email,
+  ]);
+  if (userWithEmail.length > 0)
+    return res
+      .status(400)
+      .json({ status: 'fail', message: 'user with email already exists' });
+
+  // find user with name
+  const userWithName = await query(`SELECT * FROM user WHERE name =?`, [
+    username,
+  ]);
+  if (userWithName.length > 0)
+    return res
+      .status(400)
+      .json({ status: 'fail', message: 'user with usename already exists' });
+
+  const hashedPassword = await hashPassword(password);
+
+  //create user
+  const insertResult = await query(
+    `INSERT INTO user(name, email, password) VALUES ('${username}','${email}','${hashedPassword}')`
+  );
+
+  res.status(201).json({ status: 'success', userid: insertResult.insertId });
+};
 
 exports.login = async (req, res) => {
   const { username, password } = req.body;
+
+  if (!email || !username)
+    return res
+      .status(400)
+      .json({ status: 'fail', message: 'email or username missing' });
+
+  if (!password)
+    return res
+      .status(400)
+      .json({ status: 'fail', message: 'password missing' });
 
   // find user with username
   const sql = `SELECT * FROM user WHERE email =? OR name =?`;
   const params = [username, username];
   const results = await query(sql, params);
 
-  if (results.length <= 0)
+  if (results.length)
     return res
       .status(400)
       .json({ status: 'fail', message: 'Wrong username or password' });
@@ -22,5 +76,7 @@ exports.login = async (req, res) => {
       .status(400)
       .json({ status: 'fail', message: 'Wrong username or password' });
 
+  //remove password from user and send user
+  delete user.password;
   res.status(200).json({ status: 'sucess', user });
 };
--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,9 +1,10 @@
 const { Router } = require('express');
 
-const { login } = require('../controllers/auth.controller');
+const { login, signUp } = require('../controllers/auth.controller');
 
 const router = Router();
 
 router.route('/login').post(login);
+router.route('/sign-up').post(signUp);
 
 module.exports = router;
