ff22fb9bb745731c70258d5b76d706bbe35c2a6f
Shreyansh-deep <shreyanshdeep4@gmail.com>
2023-12-10 22:37:09 +0530

    adds cors

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
 const AuthRouter = require('./routes/auth.routes');
 
 const app = express();
+
+app.use(cors({ origin: 'http://localhost:3000', credentials: true }));
 
 app.use(express.json());
 app.use(
